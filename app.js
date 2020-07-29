824b8220ece0a904d24dc1862f1e0a1dac335728
pmaity7 <dmaity897@gmail.com>
2020-07-29 15:56:27 +0530

    Added grayscale

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const clearButton = document.getElementById('reload');
 const select = document.querySelector('select');
 let selectedColor = '';
 
+
 let count = 16;
 
 let sideLength = 0;
@@ -38,25 +39,26 @@ function createBoard() {
         grid.setAttribute('style', `width: ${sideLength}px;height: ${sideLength}px; background-color: white;`);
         board.appendChild(grid);
     }
-    board.setAttribute('style', `width: ${count*sideLength}px; height: ${count*sideLength}px; grid-template-columns: repeat(${count}, 1fr); grid-template-rows: repeat(${count}, 1fr); border: 1rem solid black;`);
+    board.setAttribute('style', `width: ${count*sideLength}px; height: ${count*sideLength}px; grid-template-columns: repeat(${count}, 1fr); grid-template-rows: repeat(${count}, 1fr); border: 1rem solid black; background-color: white;`);
     const gridItem = document.getElementsByClassName('grid-item');  
     var grid = Array.from(gridItem);
     console.log(grid);
     grid.forEach(element => {
+        let opacity = 0.1;
         element.addEventListener('mouseover', () => {
             if(selectedColor == "B&W"){
                 element.style.backgroundColor = 'black';
+            }  else if (selectedColor == "Darken") {
+                element.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
             } else {
                 console.dir(element);
                 element.style.backgroundColor = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
             }
-            
-        })
+        });
+        element.addEventListener('mouseout', () => {
+            opacity = opacity + 0.1
+        });
     });
-
-    function colored() {
-        
-    }
 }
 
 
